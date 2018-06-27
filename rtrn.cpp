ed77bf750989b7629743edbffb8fe1cca4c6f4a8
imsatis <Malsonsatis@outlook.com>
2018-06-27 06:00:13 +0530

    testing

--- rtrn.cpp
+++ rtrn.cpp
@@ -5,20 +5,125 @@ using namespace std;
 
     int main()
   {
-    string a,b,c,d,e,f,g,h,i,j,k,l;
+	  string a,b,c,d,e,f,g,h,i,j,k,l;
    
-   const int count;
-       if(count==1)
+      int count=0;
+       if(a==0)
          { cin>>a;
-	   count++;
+	  }
+	 else
+	{ 
+	  count++;
 	 }
        
-       if(count==2)
+       if(b==0)
        { 
        cint>>b;
+        }
+	else
+      {	
+       count++;
+        }
+	
+        if(c==0)
+       { 
+       cint>>c;
+        }
+	else
+      {	
+       count++;
+        }
+
+        if(d==0)
+       { 
+       cint>>d;
+        }
+	else
+      {	
+       count++;
+        }
+	
+	if(e==0)
+       { 
+       cint>>e;
+        }
+	else
+      {	
+       count++;
+        }
+
+       if(f==0)
+       { 
+       cint>>f;
+        }
+	else
+      {	
+       count++;
+        }
+
+        if(g==0)
+       { 
+       cint>>g;
+        }
+	else
+      {	
+       count++;
+        }
+     
+        if(h==0)
+       { 
+       cint>>h;
+        }
+	else
+      {	
+       count++;
+        }
+
+       if(i=0)
+       { 
+       cint>>h;
+        }
+	else
+      {	
+       count++;
+        }
+
+       if(j==0)
+       { 
+       cint>>i;
+        }
+	else
+      {	
        count++;
         }
        
+        if(k==0)
+       { 
+       cint>>j;
+        }
+	else
+      {	
+       count++;
+        }
+ 
+        if(k==0)
+       { 
+       cint>>k;
+        }
+	else
+      {	
+       count++;
+        }
+
+       if(l==0)
+       { 
+       cint>>l;
+        }
+	else
+      {	
+       count++;
+        }
+
 
     cout<<a;
     cout<<b;
@@ -32,5 +137,5 @@ using namespace std;
     cout<<j;
     cout<<k;
     cout<<l;
-     
+     getch();
        }
